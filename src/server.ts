a8ba557621b79529027a7ee5b02cb9e989f98a66
brunocmg <brunocmg2006@gmail.com>
2025-10-10 12:47:20 -0300

    feat: add PUT and PATCH /movies/:id routes

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,23 @@
-import express from 'express';
-import { findAll as findAllMovies, create as createMovie, findById as findMovieById} from './controllers/movie.controller';
+import express from "express";
+import {
+  findAll as findAllMovies,
+  create as createMovie,
+  findById as findMovieById,
+  update as updateMovie,
+  patch as updatePartialMovie,
+} from "./controllers/movie.controller";
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
 
-app.get('/movies', findAllMovies);
-app.post('/movies', createMovie);
-app.get('/movies/:id', findMovieById)
+app.get("/movies", findAllMovies);
+app.post("/movies", createMovie);
+app.get("/movies/:id", findMovieById);
+app.put("/movies/:id", updateMovie);
+app.patch("/movies/:id", updatePartialMovie);
 
 app.listen(PORT, () => {
-    console.log(`Servidor rodando na porta ${PORT}`);
+  console.log(`Servidor rodando na porta ${PORT}`);
 });
-
