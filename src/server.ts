3f5d1d6dcdfe5744d5e212e3573180f48016889c
brunocmg <brunocmg2006@gmail.com>
2025-10-10 11:22:45 -0300

    feat: add GET /movies/:id route

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { findAll as findAllMovies, create as createMovie} from './controllers/movie.controller';
+import { findAll as findAllMovies, create as createMovie, findById as findMovieById} from './controllers/movie.controller';
 
 const app = express();
 const PORT = 3000;
@@ -8,6 +8,7 @@ app.use(express.json());
 
 app.get('/movies', findAllMovies);
 app.post('/movies', createMovie);
+app.get('/movies/:id', findMovieById)
 
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
