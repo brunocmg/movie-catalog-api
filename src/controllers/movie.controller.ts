7f36c41aff5267387c3315281518efda6cedbde4
brunocmg <brunocmg2006@gmail.com>
2025-10-22 18:54:16 -0300

    refactor: changing array in memory to prisma/postgres in post/create

--- src/controllers/movie.controller.ts
+++ src/controllers/movie.controller.ts
@@ -2,97 +2,100 @@ import { Request, Response } from "express";
 import { movieService } from "../service/movie.service";
 import { validate as isUuid } from "uuid";
 
-export const findAll = (req: Request, res: Response) => {
-  const movies = movieService.findAll();
+// export const findAll = (req: Request, res: Response) => {
+//   const movies = movieService.findAll();
 
-  return res.status(200).json(movies);
-};
+//   return res.status(200).json(movies);
+// };
 
-export const create = (req: Request, res: Response) => {
-  const { name, genre, year } = req.body;
+export const create = async (req: Request, res: Response) => {
+  const { name, genre, director, year } = req.body;
 
-  if (!name || !genre || !year) {
+  if (!name || !year) {
     return res.status(400).json({
-      message: "Todos os campos (name, genre, year) são obrigatórios.",
+      message: "O campo de nome e ano são obrigatórios.",
     });
   }
 
-  const newMovie = movieService.create(name, genre, year);
-
-  return res.status(201).json(newMovie);
+  try {
+    const newMovie = await movieService.create(name, genre, director, year);
+    return res.status(201).json(newMovie);
+  } catch (err) {
+    return res.status(500).json({ message: (err as Error).message });
+  }
 };
 
-export const findById = (req: Request, res: Response) => {
-  const id = req.params.id;
-  if (!id) {
-    return res.status(400).json({ message: 'Param "id" is required.' });
-  }
-  if (!isUuid(id))
-    return res.status(400).json({ message: "Invalid id format" });
-  const movie = movieService.findById(id);
-  if (!movie) {
-    return res.status(404).json({ message: `Movie with id ${id} not found.` });
-  }
-  return res.status(200).json(movie);
-};
+// export const findById = (req: Request, res: Response) => {
+//   const id = req.params.id;
+//   if (!id) {
+//     return res.status(400).json({ message: 'Param "id" is required.' });
+//   }
+//   if (!isUuid(id))
+//     return res.status(400).json({ message: "Invalid id format" });
+//   const movie = movieService.findById(id);
+//   if (!movie) {
+//     return res.status(404).json({ message: `Movie with id ${id} not found.` });
+//   }
+//   return res.status(200).json(movie);
+// };
 
-export const update = (req: Request, res: Response) => {
-  const id = req.params.id;
-  if (!id) {
-    return res.status(400).json({ message: 'Param "id" is required.' });
-  }
-  if (!isUuid(id))
-    return res.status(400).json({ message: "Invalid id format" });
+// export const update = (req: Request, res: Response) => {
+//   const id = req.params.id;
+//   if (!id) {
+//     return res.status(400).json({ message: 'Param "id" is required.' });
+//   }
+//   if (!isUuid(id))
+//     return res.status(400).json({ message: "Invalid id format" });
 
-  const { name, genre, year } = req.body;
+//   const { name, genre, year } = req.body;
 
-  if (!name || !genre || !year) {
-    return res
-      .status(400)
-      .json({ message: "Name, gender and year are required" });
-  }
+//   if (!name || !genre || !year) {
+//     return res
+//       .status(400)
+//       .json({ message: "Name, gender and year are required" });
+//   }
 
-  const updated = movieService.update(id, { name, genre, year });
-  if (!update) return res.status(404).json({ message: "Not found" });
-  return res.status(200).json(updated);
-};
+//   const updated = movieService.update(id, { name, genre, year });
+//   if (!update) return res.status(404).json({ message: "Not found" });
+//   return res.status(200).json(updated);
+// };
 
-export const patch = (req: Request, res: Response) => {
-  const id = req.params.id;
-  if (!id) {
-    return res.status(400).json({ message: 'Param "id" is required.' });
-  }
-  if (!isUuid(id))
-    return res.status(400).json({ message: "Invalid id format" });
+// export const patch = (req: Request, res: Response) => {
+//   const id = req.params.id;
+//   if (!id) {
+//     return res.status(400).json({ message: 'Param "id" is required.' });
+//   }
+//   if (!isUuid(id))
+//     return res.status(400).json({ message: "Invalid id format" });
 
-  const dto: Partial<{ name: string; genre: string; year: string }> = {};
-  if ("name" in req.body) dto.name = req.body.name;
-  if ("genre" in req.body) dto.genre = req.body.genre;
-  if ("year" in req.body) dto.year = req.body.year;
+//   const dto: Partial<{ name: string; genre: string; year: string }> = {};
+//   if ("name" in req.body) dto.name = req.body.name;
+//   if ("genre" in req.body) dto.genre = req.body.genre;
+//   if ("year" in req.body) dto.year = req.body.year;
 
-  const patched = movieService.patch(id, dto);
-  if (!patched) return res.status(404).json({ message: "Not found." });
+//   const patched = movieService.patch(id, dto);
+//   if (!patched) return res.status(404).json({ message: "Not found." });
 
-  return res.status(200).json(patched);
-};
+//   return res.status(200).json(patched);
+// };
 
-export const deleteMovie = (req: Request, res: Response) => {
-  const id = req.params.id;
-  if (!id) {
-    return res.status(400).json({ message: 'Param "id" is required.' });
-  }
-  if (!isUuid(id))
-    return res.status(400).json({ message: "Invalid id format" });
+// export const deleteMovie = (req: Request, res: Response) => {
+//   const id = req.params.id;
+//   if (!id) {
+//     return res.status(400).json({ message: 'Param "id" is required.' });
+//   }
+//   if (!isUuid(id))
+//     return res.status(400).json({ message: "Invalid id format" });
 
-  const deleted = movieService.deleteMovie(id);
-  if (!deleted) return res.status(404).json({ message: "Not found." });
+//   const deleted = movieService.deleteMovie(id);
+//   if (!deleted) return res.status(404).json({ message: "Not found." });
 
-  return res.status(200).json(deleted);
-};
+//   return res.status(200).json(deleted);
+// };
 
-export const deleteAllMovies = (req: Request, res: Response) => {
-  movieService.deleteAllMovies();
+// export const deleteAllMovies = (req: Request, res: Response) => {
+//   movieService.deleteAllMovies();
 
-  const removed = movieService.deleteAllMovies();
-  return res.status(200).json(removed);
-};
+//   const removed = movieService.deleteAllMovies();
+//   return res.status(200).json(removed);
+// };
--- src/service/movie.service.ts
+++ src/service/movie.service.ts
@@ -1,83 +1,82 @@
-import { deleteAllMovies } from "../controllers/movie.controller";
+// import { deleteAllMovies } from "../controllers/movie.controller";
 import { Movie } from "../models/movie.model";
 import { PrismaClient } from "../generated/prisma";
 import { prisma } from "../lib/prisma";
 
 class MovieService {
-  public create(name: string, genre: string, year: number): Movie {
-    const id = crypto.randomUUID();
-    const newMovie: Movie = {
-      id,
-      name,
-      genre,
-      year,
-    };
-
-    this.movies.push(newMovie);
+  public async create(
+    name: string,
+    genre: string,
+    director: string,
+    year: number
+  ) {
+    const newMovie = await prisma.movie.create({
+      data: { name, genre, director, year },
+    });
 
     return newMovie;
   }
 
-  public findAll(): Movie[] {
-    return this.movies;
-  }
+  // public findAll(): Movie[] {
+  //   return this.movies;
+  // }
 
-  public findById(id: string): Movie | undefined {
-    return this.movies.find((m) => m.id === id);
-  }
+  // public findById(id: string): Movie | undefined {
+  //   return this.movies.find((m) => m.id === id);
+  // }
 
-  public update(
-    id: string,
-    dto: { name: string; genre: string; year: number }
-  ): Movie | undefined {
-    const index = this.movies.findIndex((m) => m.id === id);
-    if (index === -1) return undefined;
+  // public update(
+  //   id: string,
+  //   dto: { name: string; genre: string; year: number }
+  // ): Movie | undefined {
+  //   const index = this.movies.findIndex((m) => m.id === id);
+  //   if (index === -1) return undefined;
 
-    const updated: Movie = {
-      id,
-      name: dto.name,
-      genre: dto.genre,
-      year: dto.year,
-    };
-    this.movies[index] = updated;
-    return updated;
-  }
+  //   const updated: Movie = {
+  //     id,
+  //     name: dto.name,
+  //     genre: dto.genre,
+  //     year: dto.year,
+  //   };
+  //   this.movies[index] = updated;
+  //   return updated;
+  // }
 
-  public patch(
-    id: string,
-    dto: Partial<{ name: string; genre: string; year: string }>
-  ): Movie | undefined {
-    const index = this.movies.findIndex((m) => m.id === id);
-    if (index === -1) return undefined;
+  // public patch(
+  //   id: string,
+  //   dto: Partial<{ name: string; genre: string; year: string }>
+  // ): Movie | undefined {
+  //   const index = this.movies.findIndex((m) => m.id === id);
+  //   if (index === -1) return undefined;
 
-    const existing = this.movies[index];
-    if (!existing) return undefined;
+  //   const existing = this.movies[index];
+  //   if (!existing) return undefined;
 
-    const merged: Movie = {
-      id: existing.id,
-      name: dto.name !== undefined ? dto.name : existing.name,
-      genre: dto.genre !== undefined ? dto.genre : existing.genre,
-      year:
-        dto.year !== undefined
-          ? (dto.year as unknown as number)
-          : existing.year,
-    };
+  //   const merged: Movie = {
+  //     id: existing.id,
+  //     name: dto.name !== undefined ? dto.name : existing.name,
+  //     genre: dto.genre !== undefined ? dto.genre : existing.genre,
+  //     year:
+  //       dto.year !== undefined
+  //         ? (dto.year as unknown as number)
+  //         : existing.year,
+  //   };
 
-    this.movies[index] = merged;
-    return merged;
-  }
+  //   this.movies[index] = merged;
+  //   return merged;
+  // }
 
-  public deleteMovie(id: string): Movie | undefined {
-    const index = this.movies.findIndex((m) => m.id === id);
-    if (index === -1) return undefined;
+  // public deleteMovie(id: string): Movie | undefined {
+  //   const index = this.movies.findIndex((m) => m.id === id);
+  //   if (index === -1) return undefined;
 
-    const [removed] = this.movies.splice(index, 1);
-    return removed;
-  }
+  //   const [removed] = this.movies.splice(index, 1);
+  //   return removed;
+  // }
 
-  public deleteAllMovies(): void {
-    this.movies.length = 0;
-  }
+  // public deleteAllMovies(): void {
+  //   this.movies.length = 0;
+  // }
 }
 
 export const movieService = new MovieService();
