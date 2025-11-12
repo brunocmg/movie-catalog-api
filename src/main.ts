9f10731a8278d970e304faab9cb0756e4470ba52
brunocmg <brunocmg2006@gmail.com>
2025-11-12 15:56:28 -0300

    feat: enable global ValidationPipe (whitelist/transform)

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { MoviesModule } from './movies/movies.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(MoviesModule);
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,
+    forbidNonWhitelisted: true,
+    transform: true,
+  }));
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
