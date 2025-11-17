75362a4301809f9bca1e254393ae38084e3e8315
brunocmg <brunocmg2006@gmail.com>
2025-11-17 18:57:47 -0300

    feat: add AuthModule, AuthService, AuthController

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,69 @@
+import { HttpException, HttpStatus, Inject, Injectable } from '@nestjs/common';
+import { SignInDto } from './dto/signin.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { HashingServiceProtocol } from './hash/hashing.service';
+import jwtConfig from './config/jwt.config';
+import type { ConfigType } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private prisma: PrismaService,
+    private readonly hashingService: HashingServiceProtocol,
+
+    @Inject(jwtConfig.KEY)
+    private readonly jwtConfiguration: ConfigType<typeof jwtConfig>,
+    private readonly jwtService: JwtService,
+  ) {
+    console.log(jwtConfiguration);
+  }
+
+  async authenticate(signInDto: SignInDto) {
+    const user = await this.prisma.user.findFirst({
+      where: {
+        email: signInDto.email,
+        active: true,
+      },
+    });
+
+    if (!user) {
+      throw new HttpException(
+        'Falha ao autenticar usuário',
+        HttpStatus.UNAUTHORIZED,
+      );
+    }
+
+    const passwordIsValid = await this.hashingService.compare(
+      signInDto.password,
+      user.passwordHash,
+    );
+
+    if (!passwordIsValid) {
+      throw new HttpException(
+        'Senha/usuário incorretos',
+        HttpStatus.UNAUTHORIZED,
+      );
+    }
+
+    const token = await this.jwtService.signAsync(
+      {
+        sub: user.id,
+        email: user.email,
+      },
+      {
+        secret: this.jwtConfiguration.secret,
+        expiresIn: this.jwtConfiguration.jwtTtl,
+        audience: this.jwtConfiguration.audience,
+        issuer: this.jwtConfiguration.issuer,
+      } as any,
+    );
+
+    return {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      token: token,
+    };
+  }
+}
