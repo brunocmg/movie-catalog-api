21894330d4e5b4cf03a094e7f88d8472443ffd8e
brunocmg <brunocmg2006@gmail.com>
2025-11-17 19:19:21 -0300

    feat: add UseGuards in Patch and Delete

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,7 +1,8 @@
-import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { AuthTokenGuard } from 'src/auth/guards/auth-token.guard';
 
 @Controller('users')
 export class UsersController {
@@ -9,27 +10,26 @@ export class UsersController {
 
   @Get(':id')
   findOneUser(@Param('id', ParseIntPipe) id: number) {
-    
-    return this.userService.findOne(id)
+    return this.userService.findOne(id);
   }
 
   @Post()
   createUser(@Body() createUserDto: CreateUserDto) {
-    return this.userService.create(createUserDto)
+    return this.userService.create(createUserDto);
   }
 
+  @UseGuards(AuthTokenGuard)
   @Patch(':id')
   updateUser(
     @Param('id', ParseIntPipe) id: number,
-    @Body() updateUserDto: UpdateUserDto
+    @Body() updateUserDto: UpdateUserDto,
   ) {
-    return this.userService.update(id, updateUserDto)
+    return this.userService.update(id, updateUserDto);
   }
 
+  @UseGuards(AuthTokenGuard)
   @Delete(':id')
-  deleteUser(
-    @Param('id', ParseIntPipe) id: number
-  ) {
-    return this.userService.delete(id)
+  deleteUser(@Param('id', ParseIntPipe) id: number) {
+    return this.userService.delete(id);
   }
 }
