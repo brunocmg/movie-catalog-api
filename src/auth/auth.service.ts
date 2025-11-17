cabcae31e77ca31266a5837ee7aa73d062341049
brunocmg <brunocmg2006@gmail.com>
2025-11-17 19:08:09 -0300

    refactor: fix auth service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -23,7 +23,6 @@ export class AuthService {
     const user = await this.prisma.user.findFirst({
       where: {
         email: signInDto.email,
-        active: true,
       },
     });
 
@@ -54,8 +53,6 @@ export class AuthService {
       {
         secret: this.jwtConfiguration.secret,
         expiresIn: this.jwtConfiguration.jwtTtl,
-        audience: this.jwtConfiguration.audience,
-        issuer: this.jwtConfiguration.issuer,
       } as any,
     );
 
