26f71efe2466fa9a738e9f854c52e54247837509
brunocmg <brunocmg2006@gmail.com>
2025-11-24 19:11:01 -0300

    feat: it - should create a new user

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -2,6 +2,7 @@ import { HashingServiceProtocol } from "src/auth/hash/hashing.service"
 import { UsersService } from "./users.service"
 import { Test, TestingModule } from "@nestjs/testing"
 import { PrismaService } from "src/prisma/prisma.service"
+import { CreateUserDto } from "./dto/create-user.dto"
 
 describe('UsersService', () => {
   let userService: UsersService
@@ -39,5 +40,39 @@ describe('UsersService', () => {
     userService = module.get<UsersService>(UsersService)
     prismaService = module.get<PrismaService>(PrismaService)
     hashingService = module.get<HashingServiceProtocol>(HashingServiceProtocol)
+  })
+
+  describe ('Create User', () => {
+    it ('should create a new user', async () => {
+      const createUserDto: CreateUserDto = {
+        email: 'bruno@teste.com',
+        name: 'Bruno',
+        password: '123123'
+      }
+
+      jest.spyOn(hashingService, 'hash').mockResolvedValue('HASH_MOCK_EXAMPLE')
+
+      const result = await userService.create(createUserDto)
+
+      expect(hashingService.hash).toHaveBeenCalled()
+      expect(prismaService.user.create).toHaveBeenCalledWith({
+        data: {
+          name: createUserDto.name,
+          email: createUserDto.email,
+          passwordHash: 'HASH_MOCK_EXAMPLE'
+        },
+        select: {
+          id: true,
+          name: true,
+          email: true
+        }
+      })
+
+      expect(result).toEqual({
+        id: 1,
+        name: createUserDto.name,
+        email: createUserDto.email
+      })
+    })
   })
 })
