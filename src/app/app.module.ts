e81b92f7f5c619fce2f5b9e74ccd7bed118fe965
brunocmg <brunocmg2006@gmail.com>
2025-11-26 19:13:05 -0300

    feat: add ConfigModule in app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,18 @@ import { Module } from '@nestjs/common';
 import { MoviesModule } from 'src/movies/movies.module';
 import { UsersModule } from 'src/users/users.module';
 import { AuthModule } from 'src/auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: process.env.NODE_ENV
+        ? `.env.${process.env.NODE_ENV}`
+        : `.env`,
+    }),
     MoviesModule,
     UsersModule,
     AuthModule,
-  ]
+  ],
 })
 export class AppModule {}
