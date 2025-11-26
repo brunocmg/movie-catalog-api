d9ef42a7894a68c9a0d8bcf1df540e25f23525b6
brunocmg <brunocmg2006@gmail.com>
2025-11-26 19:23:19 -0300

    feat: add dotenv.config and prismaService var

--- test/user.e2e-spec.ts
+++ test/user.e2e-spec.ts
@@ -6,9 +6,14 @@ import { AppModule } from '../src/app/app.module';
 import { MoviesModule } from 'src/movies/movies.module';
 import { UsersModule } from 'src/users/users.module';
 import { AuthModule } from 'src/auth/auth.module';
+import * as dotenv from 'dotenv'
+import { PrismaService } from 'src/prisma/prisma.service';
+
+dotenv.config({path: '.env.test'})
 
 describe('Users (e2e)', () => {
   let app: INestApplication<App>;
+  let prismaService: PrismaService
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
