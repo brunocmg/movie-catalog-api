cc6e3e252da05e86fb45060fa758e2ca1245a721
brunocmg <brunocmg2006@gmail.com>
2025-11-26 19:27:07 -0300

    test: add beforeAll and config beforeEach

--- test/user.e2e-spec.ts
+++ test/user.e2e-spec.ts
@@ -1,5 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import request from 'supertest';
 import { App } from 'supertest/types';
 import { AppModule } from '../src/app/app.module';
@@ -8,6 +8,8 @@ import { UsersModule } from 'src/users/users.module';
 import { AuthModule } from 'src/auth/auth.module';
 import * as dotenv from 'dotenv'
 import { PrismaService } from 'src/prisma/prisma.service';
+import { execSync } from 'child_process';
+import { ConfigModule } from '@nestjs/config';
 
 dotenv.config({path: '.env.test'})
 
@@ -15,9 +17,18 @@ describe('Users (e2e)', () => {
   let app: INestApplication<App>;
   let prismaService: PrismaService
 
+  beforeAll(() => {
+    execSync('npx prisma migrate deploy')
+  })
+  
   beforeEach(async () => {
+    execSync('cross-env DATABASE_URL=file:./dev-test.db npx prisma migrate deploy')
+
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [
+        ConfigModule.forRoot({
+          envFilePath: '.env.test',
+        }),
         MoviesModule,
         UsersModule,
         AuthModule,
@@ -25,6 +36,13 @@ describe('Users (e2e)', () => {
     }).compile();
 
     app = moduleFixture.createNestApplication();
+
+    app.useGlobalPipes(new ValidationPipe({
+      whitelist: true
+    }))
+
+    prismaService = moduleFixture.get<PrismaService>(PrismaService)
+
     await app.init();
   });
 
