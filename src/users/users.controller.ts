3663c658bca348213564fab5583de4f50d20571a
brunocmg <brunocmg2006@gmail.com>
2025-11-28 20:48:50 -0300

    feat: add Swagger API operation summaries and authentication decorators to user endpoints

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -5,22 +5,27 @@ import { UpdateUserDto } from './dto/update-user.dto';
 import { AuthTokenGuard } from 'src/auth/guards/auth-token.guard';
 import { TokenPayloadParam } from 'src/auth/param/token-payload.param';
 import { PayloadTokenDto } from 'src/auth/dto/payload-token.dto';
+import { ApiBearerAuth, ApiOperation } from '@nestjs/swagger';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly userService: UsersService) {}
 
   @Get(':id')
+  @ApiOperation({ summary: 'Find details a one user' })
   findOneUser(@Param('id', ParseIntPipe) id: number) {
     return this.userService.findOne(id);
   }
 
   @Post()
+  @ApiOperation({ summary: 'Create a new user' })
   createUser(@Body() createUserDto: CreateUserDto) {
     return this.userService.create(createUserDto);
   }
 
   @UseGuards(AuthTokenGuard)
+  @ApiBearerAuth()
+  @ApiOperation({ summary: 'Update a user' })
   @Patch(':id')
   updateUser(
     @Param('id', ParseIntPipe) id: number,
@@ -32,6 +37,8 @@ export class UsersController {
   }
 
   @UseGuards(AuthTokenGuard)
+  @ApiBearerAuth()
+  @ApiOperation({ summary: 'Delete a user' })
   @Delete(':id')
   deleteUser(
     @Param('id', ParseIntPipe) id: number,
