a9f04c0c7398990033cc0b2063cff04bb2b090c0
brunocmg <brunocmg2006@gmail.com>
2025-11-28 20:07:52 -0300

    test: add it - /users (POST) - createUser

--- test/user.e2e-spec.ts
+++ test/user.e2e-spec.ts
@@ -6,23 +6,27 @@ import { AppModule } from '../src/app/app.module';
 import { MoviesModule } from 'src/movies/movies.module';
 import { UsersModule } from 'src/users/users.module';
 import { AuthModule } from 'src/auth/auth.module';
-import * as dotenv from 'dotenv'
+import * as dotenv from 'dotenv';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { execSync } from 'child_process';
 import { ConfigModule } from '@nestjs/config';
 
-dotenv.config({path: '.env.test'})
+dotenv.config({ path: '.env.test' });
 
 describe('Users (e2e)', () => {
   let app: INestApplication<App>;
-  let prismaService: PrismaService
+  let prismaService: PrismaService;
 
   beforeAll(() => {
-    execSync('npx prisma migrate deploy')
-  })
-  
+    execSync(
+      'cross-env DATABASE_URL=postgresql://postgres:12345@localhost:5432/catalog npx prisma migrate reset --force',
+    );
+  });
+
   beforeEach(async () => {
-    execSync('cross-env DATABASE_URL=file:./dev-test.db npx prisma migrate deploy')
+    execSync(
+      'cross-env DATABASE_URL=postgresql://postgres:12345@localhost:5432/catalog npx prisma migrate reset --force',
+    );
 
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [
@@ -37,24 +41,43 @@ describe('Users (e2e)', () => {
 
     app = moduleFixture.createNestApplication();
 
-    app.useGlobalPipes(new ValidationPipe({
-      whitelist: true
-    }))
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+      }),
+    );
 
-    prismaService = moduleFixture.get<PrismaService>(PrismaService)
+    prismaService = moduleFixture.get<PrismaService>(PrismaService);
 
     await app.init();
   });
 
   afterEach(async () => {
-    await prismaService.user.deleteMany()
-  })
+    await prismaService.user.deleteMany();
+  });
 
   afterEach(async () => {
-    await app.close()
-  })
+    await app.close();
+  });
 
   describe('/users', () => {
-    
-  })
+    it('/users (POST) - createUser', async () => {
+      const createUserDto = {
+        name: 'Bruno Gomes',
+        email: 'bruno@teste.com',
+        password: '123123',
+      };
+
+      const response = await request(app.getHttpServer())
+        .post('/users')
+        .send(createUserDto)
+        .expect(201);
+
+      expect(response.body).toEqual({
+        id: response.body.id,
+        name: 'Bruno Gomes',
+        email: 'bruno@teste.com',
+      });
+    });
+  });
 });
