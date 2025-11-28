6e27713567f778d40dc26392c93788b35df3f7c8
brunocmg <brunocmg2006@gmail.com>
2025-11-28 20:55:09 -0300

    feat: add Swagger API operation summaries to movies controller methods

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -2,32 +2,41 @@ import { Controller, Get, Post, Body, Patch, Param, Delete, ParseIntPipe } from 
 import { MoviesService } from './movies.service';
 import { CreateMovieDto } from './dto/create-movie.dto';
 import { UpdateMovieDto } from './dto/update-movie.dto';
+import { ApiOperation } from '@nestjs/swagger';
 
 @Controller('movies')
 export class MoviesController {
   constructor(private readonly moviesService: MoviesService) {}
 
   @Post()
+  @ApiOperation({ summary: 'Register a new movies' })
   create(@Body() createMovieDto: CreateMovieDto) {
     return this.moviesService.create(createMovieDto);
   }
 
   @Get()
+  @ApiOperation({ summary: 'Find all movies' })
   findAll() {
     return this.moviesService.findAll();
   }
 
   @Get(':id')
+  @ApiOperation({ summary: 'Find a one movie' })
   findOne(@Param('id', ParseIntPipe) id: number) {
     return this.moviesService.findOne(id);
   }
 
   @Patch(':id')
-  update(@Param('id', ParseIntPipe) id: number, @Body() updateMovieDto: UpdateMovieDto) {
+  @ApiOperation({ summary: 'Update a movies' })
+  update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() updateMovieDto: UpdateMovieDto,
+  ) {
     return this.moviesService.update(id, updateMovieDto);
   }
 
   @Delete(':id')
+  @ApiOperation({ summary: 'Delete a movies' })
   remove(@Param('id', ParseIntPipe) id: number) {
     return this.moviesService.remove(id);
   }
