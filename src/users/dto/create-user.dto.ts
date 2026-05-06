c8d5737bf849bd059388fbf2dccd36d831e8f171
brunocmg <brunocmg2006@gmail.com>
2026-05-06 00:02:22 -0300

    fix: standardize import quotes and add missing semicolons in DTO files

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,15 +1,15 @@
-import { IsEmail, IsNotEmpty, IsString, MinLength } from "class-validator";
+import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
 
 export class CreateUserDto {
   @IsString()
   @IsNotEmpty()
-  name: string
+  name: string;
 
   @IsEmail()
-  email: string
+  email: string;
 
   @IsString()
   @MinLength(6)
   @IsNotEmpty()
-  password: string
+  password: string;
 }
